11b113124998bd9521b237042e4f6a4fb20d24d7
Wellington Viveiro <wellington@asmex.digital>
2019-04-09 17:29:05 +1000

    Add an App object for testing

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyComponenent from './components';
+
+
+export default class App extends React.Component {
+    public render() {
+
+        return <MyComponenent />
+    }
+}
